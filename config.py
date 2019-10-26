9d3e4a1766c06f991650c0f889bbc48edf6915c4
isa <fsb.off@mail.ru>
2019-10-26 19:25:21 +0300

    go to python 3.8.0

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 TOKEN = '963004625:AAHgQxjlFFiOVSx7tmeyORmWx28vy2maK8E'  # полученный у @BotFather
 PROXY = 'socks5h://user_8abd0cee:c3f33dee779316d24e597d1c8abd0cee@116.202.18.252:4002'
+# created by "conda list -e > requirements.txt"
+# conda env export > environment.yml
